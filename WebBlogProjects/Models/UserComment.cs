8c590dce9478c329ec18e15c5ecfb248cbdcf649
Talha Arslan <107929171+Lynch811@users.noreply.github.com>
2022-12-18 01:24:27 +0300

    38.Videoya kadar olanlar

---
+++ WebBlogProjects/Models/UserComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBlogProjects.Models
+{
+    public class UserComment
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+
+
+    }
+}
---
+++ WebBlogProjects/ViewComponents/CommentList.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebBlogProjects.Models;
+
+namespace WebBlogProjects.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    Username="Murat"
+                },
+                new UserComment
+                {
+                    ID=2,
+                    Username="Mesut"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username="Merve"
+                }
+            };
+
+
+            return View(commentvalues);
+        }
+
+    }
+}
